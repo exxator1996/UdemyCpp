9ec6b695b8a32a5a8b92b61a5a3b18a2a137104d
Niklas Guntelmann <niklasguntelmann@live.de>
2024-10-28 13:02:40 +0100

    ComputerGame 2 fertig

---
+++ ComputerGame2/myMain.cc
@@ -0,0 +1,55 @@
+#include <cstdint>
+#include <iostream>
+
+int main()
+{
+    char LEFT = 'a';
+    char RIGHT = 'd';
+
+    char move = 0;
+
+    std::uint16_t player_postion = 0;
+
+    const std::uint16_t field_start = 0;
+    const std::uint16_t field_end = 9;
+
+    while (player_postion < field_end)
+    {
+        //Update Screen
+        for (int i = field_start; i <= field_end; i++)
+        {
+            if (i == player_postion)
+            {
+                std::cout << "P";
+            }
+            else if (i == field_start || i == field_end)
+            {
+                std::cout << "|";
+            }
+            else
+            {
+                std::cout << ".";
+            }
+        }
+
+        //Move Player
+        std::cin >> move;
+
+        if (move == LEFT && player_postion != field_start)
+        {   player_postion--;
+            std::cout << "The Player moved to the left!" << std::endl;
+        }
+        else if (move == RIGHT && player_postion != field_end)
+        {
+            player_postion++;
+            std::cout << "The Player moved to the right!" << std::endl;
+        }
+        else
+        {
+            std::cout << "Unrecognized move!" << std::endl;
+        }
+    }
+
+    std::cout << "The Player reached the goal!" << std::endl;
+    return 0;
+}
