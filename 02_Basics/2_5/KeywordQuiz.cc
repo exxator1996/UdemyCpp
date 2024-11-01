b522e9e2e1d135c01deee085e18293b9a028271f
Niklas Guntelmann <niklasguntelmann@live.de>
2024-11-01 19:29:46 +0100

    Kapitel 2 Aufgabe 2 Fertig

--- 02_Basics/2_5/KeywordQuiz.cc
+++ 02_Basics/2_5/KeywordQuiz.cc
@@ -1,27 +1,23 @@
 #include <cstdint>
 #include <iostream>
 
-constexpr float PI = 3.14159;
+constexpr float c_PI = 3.14159;
 
-float circle_area(const float radius)
-{
-    return PI * radius * radius;
+float circle_area(const float radius) { return c_PI * radius * radius; }
+
+constexpr std::uint32_t sum(const std::uint32_t n) {
+  if (n > 1) {
+    return n + sum(n - 1);
+  }
+
+  return n;
 }
 
-constexpr std::uint32_t sum(const std::uint32_t n)
-{
-    if (n > 1)
-        return n + sum(n - 1);
+int main() {
+  constexpr std::uint32_t n = 10;
+  constexpr std::uint32_t s = sum(n);
 
-    return n;
-}
+  std::cout << "Sum from 0..10: " << s << std::endl;
 
-int main()
-{
-    constexpr std::uint32_t n = 10;
-    constexpr std::uint32_t s = sum(n);
-
-    std::cout << "Sum from 0..10: " << s << std::endl;
-
-    return 0;
+  return 0;
 }
--- 02_Basics/_Exercise2/Exercise/main.cc
+++ 02_Basics/_Exercise2/Exercise/main.cc
@@ -1,14 +1,24 @@
 #include <cstdint>
 #include <iostream>
 
-void super_duper_example()
-{
+void super_duper_example(const int input_number) {
+  static int cnt = 0;
+  constexpr int reset_value = 10;
 
+  if (input_number == reset_value) {
+    cnt = 0;
+  }
+
+  if (input_number % 2 == 0) {
+    ++cnt;
+  }
+
+  std::cout << "Counter: " << cnt << std::endl;
 }
 
-int main()
-{
-
-
-    return 0;
+int main() {
+  for (int i = 0; i < 19; i++) {
+    super_duper_example(i);
+  }
+  return 0;
 }
