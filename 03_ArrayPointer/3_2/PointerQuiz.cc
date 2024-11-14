147b0eda3d72ef083c3a3560fca8e5cbbcf2e855
Niklas Guntelmann <niklasguntelmann@live.de>
2024-11-14 21:05:26 +0100

    Kapitel 2 aufgabe 2 fertig

--- 03_ArrayPointer/3_2/PointerQuiz.cc
+++ 03_ArrayPointer/3_2/PointerQuiz.cc
@@ -6,20 +6,26 @@ int main()
     int b = 2;
     int c = 3;
 
-    int *p_d = &a;
-    int *p_e = &b;
-    int *p_f = &c;
+    int *p_d = &a; //p_d zeigt auf a
+    int *p_e = &b; //p_e zeigt auf b
+    int *p_f = &c; //p_f zeigt auf c
 
-    *p_d = (2 * (*p_e)) + (3 * (*p_f));
-    p_d = &b;
-    p_f = &a;
+    *p_d = (2 * (*p_e)) + (3 * (*p_f)); // 2 * b + 3 * c = 13 => a = 13
+    p_d = &b; //p_d zeigt auf b
+    p_f = &a; //p_f zeigt auf a
 
-    *p_e = *p_d + *p_f;
+    *p_e = *p_d + *p_f; //b = b + a = 2 + 13 = 15
 
-    std::cout << a << '\n';
-    std::cout << *p_d << '\n';
-    std::cout << *p_e << '\n';
-    std::cout << *p_f << '\n';
+    std::cout << a << '\n';  // a=13
+    std::cout << *p_d << '\n'; // b = 15
+    std::cout << *p_e << '\n'; // b = 15
+    std::cout << *p_f << '\n'; // a = 13
 
     return 0;
+
+
+
+
+
+
 }
