973273e53e9f380d2a02e53345363a7df3895ce2
Niklas Guntelmann <niklasguntelmann@live.de>
2024-11-27 15:45:44 +0100

    Kapitel 3 aufgabe 3

--- 03_ArrayPointer/_Exercise3/Exercise/exercise.cc
+++ 03_ArrayPointer/_Exercise3/Exercise/exercise.cc
@@ -1,16 +1,31 @@
+#include "exercise.h"
+
 #include <cstdint>
 #include <iostream>
 
-#include "exercise.h"
-
 // Exercise 1
-void push_back(std::int32_t *&input_array,
-               const std::size_t size,
-               const std::int32_t value)
-{
+void push_back(std::int32_t *&input_array, const std::size_t size,
+               const std::int32_t value) {
+  int *new_array = new int[size + 1];
+
+  for (std::uint32_t i = 0; i < size; i++) {
+    new_array[i] = input_array[i];
+  }
+
+  new_array[size] = value;
+
+  delete[] input_array;
+  input_array = new_array;
 }
 
 // Exercise 2
-void pop_back(std::int32_t *&input_array, const std::size_t size)
-{
+void pop_back(std::int32_t *&input_array, const std::size_t size) {
+  int *new_array = new int[size - 1];
+
+  for (std::uint32_t i = 0; i < size - 1; i++) {
+    new_array[i] = input_array[i];
+  }
+
+  delete[] input_array;
+  input_array = new_array;
 }
